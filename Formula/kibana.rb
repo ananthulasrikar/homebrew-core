fa322d02d4f80c8f2669003b350ae5cd37fa4c34
Srikar Ananthula <srikar.a@turvo.com>
2016-10-21 15:02:11 +0530

    4.6.1, 5.0.0-rc1 version upgrade for the kibana formula

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -26,7 +26,7 @@ class Kibana < Formula
 
   resource "node" do
     url "https://nodejs.org/dist/v4.4.7/node-v4.4.7.tar.gz"
-    sha256 "53c694c203ee18e7cd393612be08c61ed6ab8b2a165260984a99c014d1741414"
+    sha256 "cbe1c6e421969dd5639d0fbaa6d3c1f56c0463b87efe75be8594638da4d8fc4f"
   end
 
   def install
